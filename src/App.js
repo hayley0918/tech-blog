5ab4bbc0ddc6ac6491aa9266b8bad8c35b8416b0
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-07-21 09:47:38 +1000

    try to fix the homepage

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
       <div className="App">
         <Header />
         <Hero />
-        <Home />
-        <Route path="/" exact component={Home} />
+        <Route path="/tech-blog" exact component={Home} />
         <Route path="/about-me" component={AboutMe} />
         <Route path="/post/:postId" component={Post} />
       </div>
